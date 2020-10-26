a725ad7733c2d3a9a7f7cc71da2d785e6e427426
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-10-26 17:40:57 -0400

    feat: use current user info in navbar

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useAuth } from '../contexts/AuthContext';
 
 function NavBar() {
+  const { user } = useAuth();
+
   return (
     <div className="flex justify-between w-100 mx-1">
       <img src="/devchallenges.svg" alt="devchallenges logo" className="" />
       <div className="flex-grow flex justify-end items-center space-x-2">
         <img
-          src="/logo512.png"
+          src={user.photoURL}
           alt="avatar for user"
-          className="rounded-sm h-8 w-8"
+          className="rounded-lg h-8 w-8"
         />
-        <p className="text-sm">John Smith</p>
+        <p className="text-xs">{user.displayName}</p>
         <button>V</button>
       </div>
     </div>
