3fd7e04ab5df0f0018647e37df33a844da1bd23e
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-11-10 16:04:04 -0500

    refactor: firebase vars -> .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,16 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
-import FS_VARS from './.firebase.config.js';
 
 const firebaseConfig = {
-  apiKey: FS_VARS.apiKey,
-  authDomain: FS_VARS.authDomain,
-  databaseURL: FS_VARS.databaseURL,
-  projectId: FS_VARS.projectId,
-  storageBucket: FS_VARS.storageBucket,
-  messagingSenderId: FS_VARS.messagingSenderId,
-  appId: FS_VARS.appId,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const fbApp = firebase.initializeApp(firebaseConfig);
